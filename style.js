b7798c577fcec861d03bf20d5b3837d6836ceb82
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-05-12 13:15:08 -0500

    getting weather info but don't know how to translate

--- style.js
+++ style.js
@@ -1,88 +1,89 @@
 var userFormEl = document.querySelector('#user-form');
-var languageButtonsEl = document.querySelector('#language-buttons');
+var cityButtonsEl = document.querySelector('#city-buttons');
 var cityInputEl = document.querySelector('#city');
 var forecastContainerEl = document.querySelector('#forecast-container');
-var repoSearchTerm = document.querySelector('#repo-search-term');
+var citySearchTerm = document.querySelector('#city-search-term');
 
 var APIKey = "76f1e6ec8ad2c03d7834f08abb441680";
+// empty string to put user-input city into
 var city = [];
+ // URL to make queries to weather API
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+// function to take user input for city weather search
 var formSubmitHandler = function (event) {
   event.preventDefault();
   
-  city = nameInputEl.value.trim();
+  city = cityInputEl.value;
   
   if (city) {
     getForecast(city);
 
     forecastContainerEl.textContent = '';
-    nameInputEl.value = '';
+    cityInputEl.value = '';
   } else {
-    alert('Please enter a city');
+    alert('Please enter a city.');
   }
 };
+// TEND TO BUTTONS LATER
+// var buttonClickHandler = function (event) {
+//   // `event.target` is a reference to the DOM element of what city button was clicked on the page
+//   var preselectedCity = event.target.getAttribute('preselected-city');
 
-var buttonClickHandler = function (event) {
-  // `event.target` is a reference to the DOM element of what programming language button was clicked on the page
-  var language = event.target.getAttribute('data-language');
+//   // If there is no city read from the button, don't attempt to fetch weather
+//   if (city) {
+//     getFeaturedRepos(city);
 
-  // If there is no language read from the button, don't attempt to fetch repos
-  if (language) {
-    getFeaturedRepos(language);
+//     forecastContainerEl.textContent = '';
+//   }
+// };
 
-    forecastContainerEl.textContent = '';
-  }
-};
-
-var getForecast = function (city) {
-    // URL to make queries to weather API
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-  fetch(queryULR)
+function getForecast() {
+  
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey)
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
         response.json().then(function (data) {
           console.log(data);
-          displayRepos(data, user);
+          
+          displayForecast(data, city);
         });
       } else {
         alert('Error: ' + response.statusText);
       }
     })
     .catch(function (error) {
-      alert('Unable to connect to GitHub');
+      alert('Unable to connect to Weather');
     });
 };
 
-var getFeaturedRepos = function (language) {
-  // The `q` parameter is what language we want to query, the `+is:featured` flag adds a filter to return only featured repositories
-  // The `sort` parameter will instruct GitHub to respond with all of the repositories in order by the number of issues needing help
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+// var getFeaturedRepos = function (language) {
+//   // The `q` parameter is what language we want to query, the `+is:featured` flag adds a filter to return only featured repositories
+//   // The `sort` parameter will instruct GitHub to respond with all of the repositories in order by the number of issues needing help
+  
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         displayForecast(data.items, language);
+//       });
+//     } else {
+//       alert('Error: ' + response.statusText);
+//     }
+//   });
+// };
 
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    forecastContainerEl.textContent = 'No repositories found.';
+var displayForecast = function (city, citySearchTerm) {
+  if (city.length === 0) {
+    forecastContainerEl.textContent = 'No city found.';
     // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
     return;
   }
 
-  repoSearchTerm.textContent = searchTerm;
+//   citySearchTerm.textContent = searchTerm;
 
-  for (var i = 0; i < repos.length; i++) {
+  for (var i = 0; i < city.length; i++) {
     // The result will be `<github-username>/<github-repository-name>`
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
+    var cityName = city[i].owner.login + '/' + city[i].name;
 
     var repoEl = document.createElement('div');
     repoEl.classList = 'list-item flex-row justify-space-between align-center';
@@ -109,4 +110,4 @@ var displayRepos = function (repos, searchTerm) {
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
-languageButtonsEl.addEventListener('click', buttonClickHandler);
+// cityButtonsEl.addEventListener('click', buttonClickHandler);
