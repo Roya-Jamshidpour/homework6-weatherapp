0fd6939a4e423be432edef1c91ac448d0e593d00
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-05-13 17:08:24 -0500

    weather app is working well but I apparently exceeded calls available

--- style.js
+++ style.js
@@ -1,9 +1,10 @@
 var userFormEl = document.querySelector('#user-form');
 var cityButtonsEl = document.querySelector('#city-buttons');
 var cityInputEl = document.querySelector('#city');
-var icon = document.querySelector('#weather-icon');
+var icon = document.getElementById('#weather-icon');
 var forecastContainerEl = document.querySelector('#forecast-container');
 var citySearchTerm = document.querySelector('#city-search-term');
+let lat, lon, setUv
 
 // city buttons 
 var seattleButton = document.querySelector('#seattle')
@@ -52,11 +53,11 @@ function getForecast(city) {
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          console.log(data);
-          let lat = data.coord.lat
-          let lon = data.coord.lon
+          lon = data.coord.lon
+          lat = data.coord.lat
+          getUVIndex(lat, lon);
           displayForecast(data);
-        //   getUVIndex(lat, lon);
+        
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -67,26 +68,30 @@ function getForecast(city) {
     });
 };
 // function to get UV Index for city
-// function getUVIndex(lat, lon) {
-//     console.log(lat, lon)
-//     // THIS FETCH IS NOT WORKING FOR SOME REASON
-//     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "$&lon=$" + lon + "&units=imperial&exclude=minutely,hourly,daily,alerts&appid=" + APIKey)
-//     .then(response => response.json())
-//     console.log(data)
-//         .then(data => {
-//             let UVI = data['current']['uvi'];
-    
-// })
-// };
+function getUVIndex(lat, lon) {
+    let getUv = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
+    fetch(getUv)
+    .then(function (response) {
+        response.json().then(function (data){
+             setUv = data.value
+            displayForecast(setUv);
+        })
+    }) 
+   
+};
 // function to display forecast within a card for current day
 function displayForecast(data) {
+    // getUVIndex(lat, lon);
+    console.log(setUv);
     let currentDate = new Date();
-    let weatherIcon = data.weather[0].icon + '.png';
+    let weatherIcon = data.weather[0].icon + ".png"
     var cityName = data['name'];
     var currentTemp = data.main.temp ;
     var currentWind = data.wind.speed;
     var currentHumidity = data.main.humidity;
     console.log(cityName, weatherIcon, currentTemp, currentDate, currentWind, currentHumidity);
+
+  
     // displays name above weather display 
     citySearchTerm.innerHTML = cityName;
 
@@ -103,8 +108,9 @@ function displayForecast(data) {
     currentWeatherCardEl.appendChild(todayTitle);
 
     // append weather icon to card
-    icon.innerHTML = weatherIcon;
-    currentWeatherCardEl.appendChild(icon)
+    let weatherIMG = document.getElementById("#weather-icon")
+    weatherIMG.innerHTML = icon;
+    currentWeatherCardEl.appendChild(weatherIMG)
 
     // appended date to weather card
     let cardDate = document.createElement('span');
